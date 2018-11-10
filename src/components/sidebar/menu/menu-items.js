1e58b03097b1d16774d62256801e4b159cead081
Michael Darche <mdarche@Michaels-iMac.fios-router.home>
2018-11-10 17:11:29 -0500

    Update nav links

--- src/components/sidebar/menu/menu-items.js
+++ src/components/sidebar/menu/menu-items.js
@@ -1,32 +1,37 @@
 const Contractor = [
   {
-    label: 'Active Tasks',
-    route: '/active-tasks',
-    icon: 'fas fa-th-list'
+    label: 'Dashboard',
+    route: '/',
+    icon: 'fas fa-chart-pie'
   },
   {
-    label: 'Chat',
-    route: '/chat',
-    icon: 'fas fa-comment-alt'
+    label: 'Tasks',
+    route: '/tasks',
+    icon: 'fas fa-list'
   },
   {
-    label: 'My Files',
-    route: '/my-files',
+    label: 'Links',
+    route: '/links',
     icon: 'fas fa-paperclip'
   },
   {
-    label: 'Completed Tasks',
-    route: '/completed-tasks',
-    icon: 'fas fa-check-circle'
+    label: 'Projects',
+    route: '/projects',
+    icon: 'fas fa-clone'
   },
   {
-    label: 'My Payments',
-    route: '/payments',
-    icon: 'fas fa-money-check-alt'
+    label: 'Vote',
+    route: '/vote',
+    icon: 'fas fa-check'
   },
   {
-    label: 'My Account',
-    route: '/my-account',
+    label: 'Notifications',
+    route: '/notifications',
+    icon: 'fas fa-bell'
+  },
+  {
+    label: 'Account',
+    route: '/account',
     icon: 'fas fa-user-alt'
   }
 ];
